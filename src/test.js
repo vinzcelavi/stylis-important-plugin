cc961ed227e69979a1571d4a0ebfc402be369a9b
Vincent Bianciotto <vincent@celavi.fr>
2021-05-10 16:05:01 +0200

    Add test if property has already important

--- src/test.js
+++ src/test.js
@@ -8,4 +8,11 @@ describe('stylisImportantPlugin', () => {
     );
     expect(property).toEqual('display:block !important');
   });
+  it('doesn\'t transform display: block !important;', () => {
+    const property = stylisImportantPlugin(
+      STYLIS_PROPERTY_CONTEXT,
+      'display:block !important'
+    );
+    expect(property).toEqual('display:block !important');
+  });
 });
